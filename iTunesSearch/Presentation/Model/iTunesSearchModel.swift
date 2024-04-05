0c7650f01bff80fbebe5c27a3af95f77a9a570df
Jin0331 <sempre813@naver.com>
2024-04-05 19:33:19 +0900

    [✅Add] API Model 생성

---
+++ iTunesSearch/Presentation/Model/iTunesSearchModel.swift
@@ -0,0 +1,39 @@
+//
+//  iTunesSearchModel.swift
+//  iTunesSearch
+//
+//  Created by JinwooLee on 4/5/24.
+//
+
+import Foundation
+
+struct iTunesSearchList: Decodable {
+    let results: [iTunesSearch]
+}
+
+struct iTunesSearch : Decodable {
+    let artworkUrl60, artworkUrl100: String
+    let screenshotUrls: [String]
+    let minimumOSVersion: String
+    let averageUserRating: Double
+    let trackCensoredName, sellerName, releaseNotes: String
+    let artistID: Int
+    let artistName: String
+    let genres: [String]
+    let description: String
+    let trackID: Int
+    let trackName: String
+    let releaseDate, currentVersionReleaseDate: Date
+    let averageUserRatingForCurrentVersion: Double
+    let trackContentRating, version, wrapperType: String
+
+    enum CodingKeys: String, CodingKey {
+        case artworkUrl60, artworkUrl100, screenshotUrls
+        case minimumOSVersion = "minimumOsVersion"
+        case averageUserRating, trackCensoredName, sellerName, releaseNotes
+        case artistID = "artistId"
+        case artistName, genres, description
+        case trackID = "trackId"
+        case trackName, releaseDate, currentVersionReleaseDate, averageUserRatingForCurrentVersion, trackContentRating, version, wrapperType
+    }
+}
---
+++ iTunesSearch/Utility/NetworkManager/iTunesSearchManager.swift
@@ -0,0 +1,18 @@
+//
+//  iTunesSearchManager.swift
+//  iTunesSearch
+//
+//  Created by JinwooLee on 4/5/24.
+//
+
+import Foundation
+import Alamofire
+
+class iTunesSearchManager {
+    
+    static let shared = iTunesSearchManager()
+    
+    private init () { }
+    
+//    func callRequest<T:Decodable>(completionHandler : @escaping )
+}
